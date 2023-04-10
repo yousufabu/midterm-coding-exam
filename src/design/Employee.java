96c4b012bea7076275d9238ec162d69807062500
yousufabu <yousuf.abu1996@gmail.com>
2023-04-10 18:51:32 -0400

    Employee

--- src/design/Employee.java
+++ src/design/Employee.java
@@ -9,18 +9,60 @@ public interface Employee {
 	//and then implement these in a concrete class.
 	
 	//employeeId() will return employee id.
-	public int employeeId();
-	
+	//employeeId() will return employee id.
+	public int employeeId() ;
+
 	//employeeName() will return employee name
 	public String employeeName();
-	
+
+
 	//assignDepartment() will assign employee to departments
-    public void assignDepartment();
-	
+	public void assignDepartment();
+
 	//calculate employee salary
 	public int calculateSalary();
-	
+
 	//employee benefit
 	public void benefitLayout();
 
+	public String employeeAddress();
+
+	public String employeeAnniversary();
+}
+class Employ implements Employee{
+
+	@Override
+	public int employeeId() {
+		return 0;
+	}
+
+	@Override
+	public String employeeName() {
+		return null;
+	}
+
+	@Override
+	public void assignDepartment() {
+
+	}
+
+	@Override
+	public int calculateSalary() {
+		return 0;
+	}
+
+	@Override
+	public void benefitLayout() {
+
+	}
+
+	@Override
+	public String employeeAddress() {
+		return null;
+	}
+
+	@Override
+	public String employeeAnniversary() {
+		return null;
+	}
 }
